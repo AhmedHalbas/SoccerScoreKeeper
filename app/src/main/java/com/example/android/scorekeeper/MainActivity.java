2372b66e6008ddec7335bb88209075d8305efba2
AhmedHalbas <halbas.work@gmail.com>
2018-07-06 14:57:09 +0200

    delete unused libraries

--- app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -1,13 +1,11 @@
 package com.example.android.scorekeeper;
 
-import android.content.res.Configuration;
+
 import android.media.MediaPlayer;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
